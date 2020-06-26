771d7469a3066c4b4411afe9e3f16379b681dcce
张献秋 <1004885516@qq.com>
2020-06-26 14:16:20 +0800

    fix(login):删除默认登陆账号的显示

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -89,8 +89,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin01',
-        password: '123456'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
